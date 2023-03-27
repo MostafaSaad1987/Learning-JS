a9691e724ecb79200784abc12dbf766b7a4b82f3
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2023-03-27 20:45:53 +0200

    Queue

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ let thisIsAnother = "Yes";
 const thisIsAConstant = "k0Ns7";
 
 console.log(thisIsAVariable);
+
+// Queue. Adds an element to the back, removes one from the front.
+function nextInLine(arr, item) {
+    arr.push(item);
+    item = arr.shift();
+    return item;
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+// End of Queue.
