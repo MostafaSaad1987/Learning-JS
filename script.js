4593c3a2646ddb6085c5ae929b20b3567c6fb62e
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2023-03-27 03:22:39 +0200

    Changes

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// This is a comment.
-/*
-This
-is
-a
-comment
-as
-well.
-*/
-
 var thisIsAVariable;
 thisIsAVariable = 8;
 let thisIsAnother = "Yes";
 const thisIsAConstant = "k0Ns7";
-thisIsAnother = "Changed the var.";
 
 console.log(thisIsAVariable);
