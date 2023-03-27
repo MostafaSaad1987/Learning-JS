a5967737bc35991fc2c3188dfbb58681aeb1777e
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2023-03-27 21:28:02 +0200

    Switching to switch from non-switch.

--- script.js
+++ script.js
@@ -20,3 +20,53 @@ console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 // End of Queue.
+
+// Switching from if else to switch.
+// Using if else.
+function chainToSwitch(val) {
+    let answer = "";
+
+    if (val === "bob") {
+        answer = "Marley";
+    } else if (val === 42) {
+        answer = "The Answer";
+    } else if (val === 1) {
+        answer = "There is no #1";
+    } else if (val === 99) {
+        answer = "Missed me by this much!";
+    } else if (val === 7) {
+        answer = "Ate Nine";
+    }
+
+    return answer;
+}
+
+chainToSwitch(7);
+
+// Using switch.
+function chainToSwitch(val) {
+    let answer = "";
+
+    switch (val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+
+    return answer;
+}
+
+chainToSwitch(7);
+// End of switching to switch.
